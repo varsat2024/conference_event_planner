fdb79db7f05ffb0e98260b9303285fdb3de19501
varsat2024 <146130374+varsat2024@users.noreply.github.com>
2025-06-19 17:20:35 -0400

    Update ConferenceEvent.jsx

--- src/ConferenceEvent.jsx
+++ src/ConferenceEvent.jsx
@@ -54,13 +54,18 @@ const ConferenceEvent = () => {
     const calculateTotalCost = (section) => {
         let totalCost = 0;
         if (section === "venue") {
-          venueItems.forEach((item) => {
+        venueItems.forEach((item) => {
             totalCost += item.cost * item.quantity;
-          });
+        });
+        } else if (section === "av") {
+        avItems.forEach((item) => {
+            totalCost += item.cost * item.quantity;
+        });
         }
         return totalCost;
-      };
+    };
     const venueTotalCost = calculateTotalCost("venue");
+    const avTotalCost = calculateTotalCost("av");
 
     const navigateToProducts = (idType) => {
         if (idType == '#venue' || idType == '#addons' || idType == '#meals') {
@@ -176,7 +181,7 @@ const ConferenceEvent = () => {
                                     </div>
                                 ))}
                                 </div>
-                                <div className="total_cost">Total Cost:</div>
+                                <div className="total_cost">Total Cost: ${avTotalCost}</div>
 
                             </div>
 
